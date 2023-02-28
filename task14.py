c2e692a7f1a64c715ca5a7bddeba493174c5f6c7
Docvel <docvel@yandex.ru>
2023-02-28 21:06:32 +0300

    task 14 is solved

--- task14.py
+++ task14.py
@@ -1,4 +1,11 @@
-
+import checkingInput
 
 # Требуется вывести все целые степени двойки (т.е. числа вида 2**k),
 # не превосходящие числа N.
+
+print("Введите число: ", end="")
+num = checkingInput.correct_int()
+num_degree = 0
+while 2 ** num_degree <= num:
+    print(2 ** num_degree, end=" ")
+    num_degree += 1
